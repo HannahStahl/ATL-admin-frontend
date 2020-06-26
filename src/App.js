a4ff6ded7eb30571df7ef082a12e260d93cf0de7
Hannah Stahl <hls62@cornell.edu>
2020-06-26 16:12:29 -0500

    Put all styling into index.css to reduce number of files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import ErrorBoundary from "./components/ErrorBoundary";
 import { AppContext } from "./libs/contextLib";
 import { onError } from "./libs/errorLib";
 import Routes from "./Routes";
-import "./App.css";
 import config from './config';
 
 function App() {
